de955cd71cdd6a68f51782a7dd8b8c16862a36ef
Hui Peng Hu <woohp135@gmail.com>
2017-07-22 21:59:04 -0700

    update to keras 2

--- mnist.py
+++ mnist.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from keras.layers import Input
 from keras.layers.core import Dense, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.models import Model
 
 
@@ -17,9 +17,9 @@ def main():
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=60000)
 
     layers = [
-        Convolution2D(20, 3, 3, border_mode='same', activation='relu'),
+        Conv2D(20, (3, 3), padding='same', activation='relu'),
         MaxPooling2D(),
-        Convolution2D(50, 3, 3, border_mode='same', activation='relu'),
+        Conv2D(50, (3, 3), padding='same', activation='relu'),
         MaxPooling2D(),
         Flatten(),
         Dense(500, activation='relu'),
@@ -30,7 +30,7 @@ def main():
     output = toolz.pipe(input, *layers)
     model = Model(input, output)
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X_train, Y_train, batch_size=128, validation_data=[X_test, Y_test])
+    model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=[X_test, Y_test])
 
 
 if __name__ == '__main__':
